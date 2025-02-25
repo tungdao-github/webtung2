99279fa09efd467b739208c679fcece7f22aa47a
phong <phongnguyengia82@gmail.com>
2025-02-25 23:57:09 +0700

    add PasswordHelper for random password

---
+++ Helper/PasswordHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WebApplication2.Controllers;
+
+public static class PasswordHelper
+{
+    public static string GenerateRandomPassword(int length)
+    {
+        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var password = new StringBuilder();
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            byte[] buffer = new byte[1];
+            for (int i = 0; i < length; i++)
+            {
+                rng.GetBytes(buffer);
+                char randomChar = chars[buffer[0] % chars.Length];
+                password.Append(randomChar);
+            }
+        }
+        return password.ToString();
+    }
+}
