865cf86b5703dfb587e1fb179521bda522ff381b
dao van tung <tungdao123pzo@gmail.com>
2025-03-21 23:31:22 +0700

    Add Image.cs

---
+++ Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication2.Models
+{
+    
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }  // Đường dẫn ảnh
+
+        public string Description { get; set; }  // Mô tả ảnh
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Thời gian tạo
+    }
+}
